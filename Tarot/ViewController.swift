59093a7e2354fa656d6c4eda0b27518c8c8b0679
Naomi lee <lyc0503@gmail.com>
2020-11-22 17:32:10 +0800

    Initial Commit

---
+++ Tarot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tarot
+//
+//  Created by APPLE on 2020/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
